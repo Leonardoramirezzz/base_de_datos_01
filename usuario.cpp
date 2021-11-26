803a8abe9ab38e8a1e8b6397543e130991ab916c
Chega123 <85765351+Chega123@users.noreply.github.com>
2021-11-25 23:13:26 -0500

    Update usuario.cpp

--- usuario.cpp
+++ usuario.cpp
@@ -1,18 +1,61 @@
-#include "usuario.h"
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include "Tienda.h"
+#include "Usuarios.h"
 
-Usuario::Usuario(){}
+Usuario::Usuario(){
+}
 
-Usuario::Usuario (string _usuario, string _contraseña, int _edad, string _region){
+Usuario::Usuario (string _usuario, string _password, string _edad, string _region){
   usuario = _usuario;
-  contraseña = _contraseña;
+  password = _password;
   edad = _edad;
   region = _region;
+  sesion_iniciada = false;
 }
 
-void Usuario::iniciar_sesion(){
-  
+void Usuario::iniciar_sesion(string _usuario, string _password){
+  if (usuario==_usuario && password==_password){
+    sesion_iniciada = true;
+    cout<<"Sesion iniciada correctamente"<<endl<<endl;
+  }
+  else{
+    cout<<"Usuario y/o contraseñas erróneas "<<endl;
+    cout<<"Sesion no iniciada"<<endl;
+    sesion_iniciada = false;
+  }
 }
-
 void Usuario::cerrar_sesion(){
-  
+  if (sesion_iniciada==true){
+    cout<<"La sesion ha concluido"<<endl;
+    sesion_iniciada = false;
+  }
+  else{
+    cout<<"No se ha iniciado sesion"<<endl;
+  }
+}
+void Usuario::set_usuario(string _usuario){
+  usuario = _usuario;
+}
+void Usuario::set_password(string _password){
+  password = _password;
+}
+void Usuario::set_region (string _region){
+  region = _region;
+}
+void Usuario::set_edad(string _edad){
+  edad = _edad;
+}
+string Usuario::get_region(){
+  return region;
+}
+string Usuario::get_edad(){
+  return edad;
+}
+string Usuario::get_usuario(){
+  return usuario;
+}
+string Usuario::get_contraseña(){
+  return password;
 }
