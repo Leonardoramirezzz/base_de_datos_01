ec86e4fd9cd833989e607ee5ac09ed172759d457
SuperD999 <91023394+SuperD999@users.noreply.github.com>
2021-11-25 23:16:47 -0500

    Update usuario_general.h

--- usuario_general.h
+++ usuario_general.h
@@ -1,10 +1,11 @@
-#include "cliente_empleado.h"
+#pragma once
+#include "empleado.h"
 
-//using namespace std;
+using namespace std;
 
 class Administrador: public Empleado {
   public:
-    Administrador (std::string _usuario, std::string _contraseña, int _edad, std::string _region, int _id);
+    Administrador (std::string _usuario, std::string _contraseña, string _edad, std::string _region, int _id);
     void ver_compras();
     void ver_datos();
     void ver_estadisticas();
