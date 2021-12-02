40d4eee711b149ff3d614c990f35819f80d9fbdb
RodrigoMelendez <85767911+RodrigoMelendezFlores@users.noreply.github.com>
2021-12-02 09:32:47 -0500

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Usuarios.h"
-
+#include "producto.h"
+#include "empleado.h"
 using namespace std;
 
 class Cliente: public Usuario {
@@ -9,8 +10,8 @@ class Cliente: public Usuario {
     Cliente (std::string _usuario, std::string _contraseña, std::string edad, std::string _region);
     void ver_perfil();
     void ver_catalogo();
-    void comprar();
+    void comprar(int nume_productos, Producto* Lista_Productos);
     void quejas_reclamos();
     void ayuda_cliente();
+    void iniciar_sesion(std::string _usuario, std:: string _password,int nume_clientes, Cliente* listaC,Empleado* ListaEm);
 };
-
