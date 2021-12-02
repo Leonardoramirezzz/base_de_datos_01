f548aa753a84f92bd13aedd817e4a8c8ec9b0d80
Chega123 <85765351+Chega123@users.noreply.github.com>
2021-12-02 09:33:05 -0500

    Update Usuarios.h

--- Usuarios.h
+++ Usuarios.h
@@ -7,14 +7,13 @@ using namespace std;
 class Usuario {
   private:
     std::string usuario, password, region, edad;
-    //int edad;
+  
   public:
     bool sesion_iniciada;
   public:
     Usuario();
     Usuario (std::string _usuario, std::string _password, string _edad, std::string _region);
-    void iniciar_sesion(std::string _usuario,std::string _password);
-    void cerrar_sesion();
+    
     void set_usuario(std::string _usuario);
     void set_password(std::string _password);
     void set_region (std::string _region);
