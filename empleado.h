8a2473e6bd2dd2c5fff3bf76a6d9ff40202315a3
RodrigoMelendez <85767911+RodrigoMelendezFlores@users.noreply.github.com>
2021-12-02 09:33:43 -0500

    Update empleado.h

--- empleado.h
+++ empleado.h
@@ -6,7 +6,8 @@ using namespace std;
 class Empleado: public Usuario {
   public:  
     int id;
+    Empleado();
     Empleado (std::string _usuario, std::string _contraseña, string _edad, std::string _region, int _id);
-    void ver_almacen();
+    void ver_almacen(string region);
     void ver_quejas();
 };
