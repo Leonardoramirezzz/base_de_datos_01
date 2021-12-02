907436caa999346b5e596dd955e160879fb64405
SuperD999 <91023394+SuperD999@users.noreply.github.com>
2021-12-02 09:33:07 -0500

    Update cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -1,6 +1,10 @@
 #include "cliente.h"
+#include "Interfaz.h"
+#include "empleado.h"
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <string.h>
 
 ofstream Guardar_2("Quejas_Cliente.txt", ios::app);
 
@@ -22,23 +26,40 @@ void Cliente::ver_perfil(){
 
 void Cliente::ver_catalogo(){
   string texto;
-  Lec_cliente.open("inventario.txt", ios::in);
+  string nombre_inventario;
+  nombre_inventario = "Inventario_" + get_region() + ".txt";
+  int count = 1;
+  Lec_cliente.open(nombre_inventario.c_str(), ios::in);
   if(Lec_cliente.fail()) cout << "Error al abrir el archivo" << endl;
-  while (!Lec_cliente.eof()){   // Mientras no se el final del archivo
+  getline(Lec_cliente,texto);
+  cout << "\n\n---------------------  GAMESPOT  ---------------------" << endl;
+  
+  while (!Lec_cliente.eof()){   // Mientras no se el final del archivo 
     getline(Lec_cliente,texto);
-    cout << texto << endl;
+    cout << count << ". " << texto << endl;
+    count++;
   }
   Lec_cliente.close(); 
+
 }
 
-void Cliente::comprar(){
-  // Falta
+void Cliente::comprar(int nume_productos, Producto* Lista_Productos){
+  int indice = nume_productos-1, nueva_cantidad;
+  if (stoi(Lista_Productos[indice].get_cantidad())> 0){
+    nueva_cantidad = (stoi(Lista_Productos[indice].get_cantidad()))-1;
+    Lista_Productos[indice].set_cantidad(to_string(nueva_cantidad)); 
+    
+    cout << "Producto escogido: " << Lista_Productos[indice].get_nombre();
+    cout << "\n¡La compra fue hecha con éxito!" << endl;
+  }
+  else cout << "El producto está agotado" << endl;
 }
+
 
 void Cliente::quejas_reclamos(){
   string reclamo;
   cout << "Escriba su reclamo: " << endl;
-  getline(cin,reclamo);
+  cin >> reclamo;
   Guardar_2 << "Nuevo reclamo: " << reclamo << endl;
 }
 
@@ -54,3 +75,34 @@ void Cliente::ayuda_cliente(){
   Lec_cliente.close(); 
 }
 
+
+void Cliente::iniciar_sesion(std::string _usuario, std:: string _password,int nume_clientes, Cliente* listaC,Empleado* ListaEm){
+  if(_usuario== "Gamespot2021" && _password =="powertotheplayers" ) {
+    std::cout<<"Bienvenido jefecito"<<std::endl;
+    sesion_iniciada = true;
+    menu_admin();
+    }
+  for (int k=0;k<5;k++){
+    if (ListaEm[k].get_usuario()== _usuario&& ListaEm[k].get_contraseña()==_password){
+    std::cout<<"Sesion iniciada correctamente"<<std::endl<<"Bienvenido de vuelta nuestro valioso y queridisimo empleado  "<<ListaEm[k].get_usuario()<<std::endl;
+    sesion_iniciada = true;
+    menu_empleado(ListaEm[k]);
+    }
+
+  }
+  for (int i=0;i<nume_clientes;i++)
+  {
+    if(listaC[i].get_usuario()==_usuario && listaC[i].get_contraseña()==_password)
+    {
+      std::cout<<"Sesion iniciada correctamente, disfute su visita a GameSpot "<<listaC[i].get_usuario()<<std::endl;
+      sesion_iniciada = true;
+      menu_cliente(listaC[i]);
+    } 
+  }
+
+
+  if(sesion_iniciada==false){
+      std::cout<<"Sesion no encontrada por favor registrese para acceder a la plataforma"<<endl;
+  }
+  
+}
