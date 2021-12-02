fd672270efba068f0ea40cc2f84bd99586c1d271
Chega123 <85765351+Chega123@users.noreply.github.com>
2021-12-02 09:32:43 -0500

    Update Usuarios.cpp

--- Usuarios.cpp
+++ Usuarios.cpp
@@ -15,26 +15,6 @@ Usuario::Usuario (string _usuario, string _password, string _edad, string _regio
   sesion_iniciada = false;
 }
 
-void Usuario::iniciar_sesion(string _usuario, string _password){
-  if (usuario==_usuario && password==_password){
-    sesion_iniciada = true;
-    cout<<"Sesion iniciada correctamente"<<endl<<endl;
-  }
-  else{
-    cout<<"Usuario y/o contraseñas erróneas "<<endl;
-    cout<<"Sesion no iniciada"<<endl;
-    sesion_iniciada = false;
-  }
-}
-void Usuario::cerrar_sesion(){
-  if (sesion_iniciada==true){
-    cout<<"La sesion ha concluido"<<endl;
-    sesion_iniciada = false;
-  }
-  else{
-    cout<<"No se ha iniciado sesion"<<endl;
-  }
-}
 void Usuario::set_usuario(string _usuario){
   usuario = _usuario;
 }
