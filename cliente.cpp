cf8810c182fc45da6aaa2096212231f9af107fa5
SuperD999 <91023394+SuperD999@users.noreply.github.com>
2021-12-05 19:35:18 -0500

    Update cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -11,9 +11,13 @@ ofstream Guardar_2("Quejas_Cliente.txt", ios::app);
 ifstream Lec_cliente;
 
 
-Cliente::Cliente(){}
+Cliente::Cliente(){
+  num_compras = "0";
+}
 
-Cliente::Cliente (string _usuario, string _contraseña, string _edad, string _region) : Usuario(_usuario, _contraseña, _edad, _region){}
+Cliente::Cliente (string _usuario, string _contraseña, string _edad, string _region, string _num_compras) : Usuario(_usuario, _contraseña, _edad, _region){
+  num_compras = _num_compras;
+}
 
 void Cliente::ver_perfil(){
   cout << "\n--------- GameSpot ---------" << endl;
@@ -22,6 +26,7 @@ void Cliente::ver_perfil(){
   cout << "Contraseña: " << get_contraseña() << endl;
   cout << "Edad: " << get_edad() << endl;
   cout << "Región: " << get_region() << endl;
+  cout << "Número de compras: " << get_num_compras() << endl;
 }
 
 void Cliente::ver_catalogo(){
@@ -43,16 +48,27 @@ void Cliente::ver_catalogo(){
 
 }
 
-void Cliente::comprar(int nume_productos, Producto* Lista_Productos){
+void Cliente::comprar(int num_clientes,Cliente *Lista_clientes, int nume_productos, Producto* Lista_Productos,int i){
   int indice = nume_productos-1, nueva_cantidad;
   if (stoi(Lista_Productos[indice].get_cantidad())> 0){
     nueva_cantidad = (stoi(Lista_Productos[indice].get_cantidad()))-1;
     Lista_Productos[indice].set_cantidad(to_string(nueva_cantidad)); 
-    
+    Lista_clientes[i].set_num_compras(to_string(stoi(num_compras)+1));
+    actualizar(num_clientes, Lista_clientes);
     cout << "Producto escogido: " << Lista_Productos[indice].get_nombre();
     cout << "\n¡La compra fue hecha con éxito!" << endl;
   }
   else cout << "El producto está agotado" << endl;
+
+}
+
+void Cliente::actualizar(int num_clientes,Cliente* Lista_clientes){
+  remove("Datos_Usuarios.txt");  
+  ofstream actualization("Datos_Usuarios.txt",ios::out);
+  for(int i = 0; i <= num_clientes - 1; ++i){
+    actualization << Lista_clientes[i].get_usuario() << " " << Lista_clientes[i].get_contraseña() << " " << Lista_clientes[i].get_edad() <<" " <<Lista_clientes[i].get_region() << " "<<Lista_clientes[i].get_num_compras() << endl; 
+  }
+
 }
 
 
@@ -96,7 +112,7 @@ void Cliente::iniciar_sesion(std::string _usuario, std:: string _password,int nu
     {
       std::cout<<"Sesion iniciada correctamente, disfute su visita a GameSpot "<<listaC[i].get_usuario()<<std::endl;
       sesion_iniciada = true;
-      menu_cliente(listaC[i]);
+      menu_cliente(listaC[i],i);
     } 
   }
 
@@ -105,4 +121,12 @@ void Cliente::iniciar_sesion(std::string _usuario, std:: string _password,int nu
       std::cout<<"Sesion no encontrada por favor registrese para acceder a la plataforma"<<endl;
   }
   
+}
+
+string Cliente :: get_num_compras(){
+  return num_compras;
+}
+
+void Cliente::set_num_compras(string _num_compras){
+  num_compras = _num_compras;
 }
