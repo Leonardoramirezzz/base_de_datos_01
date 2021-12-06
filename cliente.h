20001d6ae7f8bcab08ac04829262c4f8364ea582
SuperD999 <91023394+SuperD999@users.noreply.github.com>
2021-12-05 19:35:42 -0500

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -1,17 +1,16 @@
-#pragma once
-#include "Usuarios.h"
-#include "producto.h"
-#include "empleado.h"
-using namespace std;
-
 class Cliente: public Usuario {
+  private:
+    string num_compras;
   public:
     Cliente();
-    Cliente (std::string _usuario, std::string _contraseña, std::string edad, std::string _region);
+    Cliente (std::string _usuario, std::string _contraseña, std::string edad, std::string _region, string _num_compras);
     void ver_perfil();
+    string get_num_compras();
+    void set_num_compras(string _num_compras);
     void ver_catalogo();
-    void comprar(int nume_productos, Producto* Lista_Productos);
+    void comprar(int num_clientes,Cliente *Lista_clientes, int nume_productos, Producto* Lista_Productos,int i);
     void quejas_reclamos();
     void ayuda_cliente();
     void iniciar_sesion(std::string _usuario, std:: string _password,int nume_clientes, Cliente* listaC,Empleado* ListaEm);
+    void actualizar(int num_clientes,Cliente* Lista_clientes);
 };
