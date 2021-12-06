ce884417e0faada81a6e345be6ef9da098bcb2ae
RodrigoMelendez <85767911+RodrigoMelendezFlores@users.noreply.github.com>
2021-12-05 22:20:12 -0500

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,30 +1,14 @@
-#include "admin.h"
-#include <iostream>
-#include <fstream>
+#pragma once
+#include "empleado.h"
 
+using namespace std;
 
-ifstream Lec3;
-
-Administrador::Administrador (string _usuario, string _contraseña, string _edad, string _region, int _id,int _num_empleados) : Empleado(_usuario, _contraseña, _edad, _region, _id) {
-  num_empleados = _num_empleados;
-}
-void Administrador::Ver_cantidad_de_empleados(){
-  cout<< "actualmente posee "<< get_num_empleados() <<" empleados en su empresa"<<endl;
-}
-
-void Administrador::ver_datos(){
-  string texto;
-  Lec3.open("Datos_Usuarios.txt", ios::in);
-  if(Lec3.fail()) cout << "Error al abrir el archivo" << endl;
-
-  while (!Lec3.eof()){   // Mientras no se el final del archivo
-    getline(Lec3,texto);
-    cout << texto << endl;
-  }
-  Lec3.close(); 
-}
-
-int Administrador::get_num_empleados(){
-  return num_empleados;
-}
-
+class Administrador: public Empleado {
+  private:
+    int num_empleados;
+  public:
+    Administrador (std::string _usuario, std::string _contraseña, string _edad, std::string _region, int _id,int num_empleados);
+    void Ver_cantidad_de_empleados();
+    void ver_datos();
+    int get_num_empleados();
+};
